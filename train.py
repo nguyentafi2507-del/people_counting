bd4550831da838bd673eefe75a7ba4cda469d4cf
tai-24122051 <tai25072006@gmail.com>
2026-05-11 23:42:30 +0700

    update gitignore

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 def train():
     # Load a model
-    model = YOLO("runs/detect/train/weights/last.pt")
+    model = YOLO("yolo8n.pt")
 
     # Train the model
     results = model.train(data="dataset/data.yaml", epochs=50, imgsz=640, batch = 16, workers=0, cache= False, resume= True)
